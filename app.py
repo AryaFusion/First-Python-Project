95364c797dee8d3516efed0dab8b635eca141c6d
AryaFusion <aryamaner2329@gmail.com>
2026-06-17 02:02:38 +0530

    Added search endpoint

--- app.py
+++ app.py
@@ -22,3 +22,19 @@ def trending():
         })
 
     return shows
+
+@app.get("/search/{show_name}")
+def search_show(show_name: str):
+    data = get_shows()
+
+    results = []
+
+    for show in data:
+        if show_name.lower() in show["name"].lower():
+            results.append({
+                "name": show["name"],
+                "rating": show["rating"]["average"],
+                "genres": show["genres"]
+            })
+
+    return results
